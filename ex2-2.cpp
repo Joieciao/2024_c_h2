7d83abdb31c665bf097d9b66a780b9c6f111c8b7
Joieciao <joieciao.yen@gmail.com>
2024-10-24 20:22:33 +0800

    Create ex2-2.cpp

---
+++ ex2-2.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello world\n\tThis sentence will work\n");
+    
+    printf("This sentence will work\n\5.Hi");
+    
+    printf("Hi\n");
+    
+    printf("Happy\n");
+    return 0;
+}
