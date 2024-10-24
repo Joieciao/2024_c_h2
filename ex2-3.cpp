8373da1eb1cbdc4078de08db8c2eae5f5a662e78
Joieciao <joieciao.yen@gmail.com>
2024-10-24 20:39:01 +0800

    Create ex2-3.cpp

---
+++ ex2-3.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+        
+        // this is comment by Joie
+        printf("Hello World"); // this is comment
+        //----------------------------
+        
+        return.0;
+
+}
