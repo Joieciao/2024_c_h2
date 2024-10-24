c820bd09fdb615c074e680837dd23c24cc201727
Joieciao <joieciao.yen@gmail.com>
2024-10-24 20:42:13 +0800

    Create ex2-4.cpp

---
+++ ex2-4.cpp
@@ -0,0 +1,16 @@
+/*
+Welcome to GBD Online
+*/
+#include <stdio.h>
+
+int main()
+{
+        
+        // this is comment by Joie
+        printf("Hello World\n"); // this is comment
+        //----------------------------
+        
+        //printf("Hello World output 2")//this is comment
+        return.0;
+
+}
