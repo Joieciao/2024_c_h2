73e5855d2fb33a77a705d10a7106b80c0bc43d3c
Joie <joieciao.yen@gmail.com>
2024-11-07 20:28:50 +0800

    Create ex3-4.cpp

---
+++ ex3-4.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+  //char a = 65, b = 66, c = 67;
+  char a = 'a', b = 'b', c = 'c';
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+    printf("c = %d\n", c);
+    
+    return 0;
+}
