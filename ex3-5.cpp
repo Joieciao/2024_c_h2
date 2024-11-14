3eeddd373edf14deb87488364351e08b2d50f5c9
Joie <joieciao.yen@gmail.com>
2024-11-14 19:13:08 +0800

    Update ex3-5.cpp

--- ex3-5.cpp
+++ ex3-5.cpp
@@ -8,7 +8,10 @@ int main()
     scanf("%d",&x);
     printf("please input int y=");
     scanf("%d",&y);
-    printf("x+y= %d",x+y); // x-y ,x*y ,x/y
+    printf("x+y= %d\n",x+y); // x-y ,x*y ,x/y
+    printf("x-y= %d\n",x-y);
+    printf("x*y= %d\n",x*y);
+    printf("x/y= %d\n",x/y);
     
     return 0;
 }
