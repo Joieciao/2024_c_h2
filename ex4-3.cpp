d25e3f55820e2e1acd1a6d0315d1804d6971b884
Joie <joieciao.yen@gmail.com>
2024-11-14 20:30:38 +0800

    Create ex4-3.cpp

---
+++ ex4-3.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    int x,y;
+    
+    printf("please input int x=");
+    scanf("%d",&x);
+    
+    switch(x % 7){
+        case 0:
+        printf("sunday \n");
+        break;
+        case 1:
+        printf("monday \n");
+        break;
+        case 2:
+        printf("tuesday \n");
+        break;
+        case 3:
+        printf("wednesday \n");
+        break;
+        case 4:
+        printf("thursday \n");
+        break;
+        case 5:
+        printf("friday \n");
+        break;
+        case 6:
+        printf("saturday \n");
+        break;
+    }
+    
+    //(x < 18) ? printf("Good morning.") : printf("Good evening.");
+    return 0;
+}
