a03c9c16cc27f249c70ad564f7760c1966e5b567
Joie <joieciao.yen@gmail.com>
2024-11-21 20:21:03 +0800

    Create ex5-5.cpp
    1.輸入2個整數，求兩數的平方和並輸出。

---
+++ ex5-5.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int x,y;
+    
+    printf("please input int x=");
+    scanf("%d",&x);
+    printf("please input int y=");
+    scanf("%d",&y);
+    printf("x*x= %d\n",x*x);
+    printf("y*y= %d\n",y*y);
+    return 0;
+}
