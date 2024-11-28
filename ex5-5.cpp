f06ad7d9fd680dc1dc5b5ddc9d25f3d58bf952c1
Joie <joieciao.yen@gmail.com>
2024-11-28 20:42:45 +0800

    Update ex5-5.cpp

--- ex5-5.cpp
+++ ex5-5.cpp
@@ -1,3 +1,4 @@
+我的寫法
 #include <stdio.h>
 
 int main()
@@ -11,5 +12,28 @@ int main()
    
     printf("面積 = %d\n",面積);
     printf("周長 = %d\n",周長);
+    return 0;
+}
+
+
+
+老師的寫法
+#include <iostream>
+
+int main()
+{
+    int r;
+    printf("please input 圓半徑:");
+    scanf("%d",&r);
+    
+    if(r>=0){
+        printf("圓的面積 = %f",3.14*r*r);
+        printf("圓的周長 = %f",2*3.14*r);
+        
+    }else{
+        printf("r < 0 is wrong");
+    }
+    
+    
     return 0;
 }
